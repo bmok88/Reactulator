110e4531e07f930d9e61db5fe91b2d8d5060d0aa
Brendan Mok <bmok88@gmail.com>
2017-09-21 09:23:50 -0700

    Add calculator container

---
+++ src/containers/Calculator.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return <section id="calculator">Calculator</section>;
+  }
+}
+
+export default Calculator;
