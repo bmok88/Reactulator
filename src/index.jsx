ca394991520fcf43b66316a8189705f8a2962a69
Brendan Mok <bmok88@gmail.com>
2017-09-21 09:20:03 -0700

    Set-up index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,16 @@
-index.jsx;
+import React from 'react';
+import { render } from 'react-dom';
+
+import Calculator from './containers/Calculator';
+
+const renderApp = () => {
+  render(<Calculator />, document.getElementById('app'));
+};
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept('./containers/Calculator', () => {
+    renderApp();
+  });
+}
