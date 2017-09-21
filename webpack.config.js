a76640c7d8a505c5d3ec03ac33dd6dd5b205fd13
Brendan Mok <bmok88@gmail.com>
2017-09-21 09:14:26 -0700

    Set-up dev environment and index.html

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    publicPath: '/public/',
+    historyApiFallback: true,
+    compress: true,
+    open: true,
+    hot: true
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
